6e1cb538ed87eee4e9becb07af53e1961b97feb4
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2024-04-04 21:56:01 +0200

    fix contact form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,7 +13,6 @@ function Form() {
       className="flex flex-col gap-2 w-full text-white"
       data-netlify="true"
       onSubmit="submit"
-      action="/contact?success=true"
     >
       <input type="hidden" name="form-name" value="contact" />
       <label htmlFor="name">
